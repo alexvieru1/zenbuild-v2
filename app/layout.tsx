20577a8dc15b9424dafdefeb9e47a1bb20d9ce31
alexvieru1 <vieru.alexandruu@gmail.com>
2024-09-03 15:39:07 +0300

    metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = FontSans({
 });
 export const metadata: Metadata = {
   title: "Zenbuild",
-  description: "v2.0",
+  description: "Dezvoltari imobiliare, inchiriere utilaje, renovari",
 };
 
 export default function RootLayout({
